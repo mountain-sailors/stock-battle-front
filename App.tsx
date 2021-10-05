c37e4a5db5ad8d26a925ffec545cb1ca595fca98
jiyoung.lim <swimjiy@gmail.com>
2021-10-06 02:07:52 +0900

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import HomeScreen from './app/screens/HomeScreen';
+import MypageScreen from './app/screens/MypageScreen';
+import theme from './app/config/theme';
+
+const Tab = createBottomTabNavigator();
+
+const tabBarIconDomain = {
+  '홈': 'home',
+  '내 정보': 'user',
+} as const;
+type tabBarIcon = keyof typeof tabBarIconDomain;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesignIcon
+              name={tabBarIconDomain[route.name as tabBarIcon]}
+              size={size}
+              color={color}
+            />;
+          },
+          tabBarActiveTintColor: theme.colors.black,
+          tabBarInactiveTintColor: theme.colors.gray[400],
+        })}
+      >
+        <Tab.Screen
+          name="홈"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          name="내 정보"
+          component={MypageScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
