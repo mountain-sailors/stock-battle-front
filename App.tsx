e51cf16d0b964bb12c439a675c92b14d369ac9db
jiyoung.lim <swimjiy@gmail.com>
2021-10-06 03:54:37 +0900

    build: set styled component

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import { ThemeProvider } from 'styled-components/native';
 import HomeScreen from './app/screens/HomeScreen';
 import MypageScreen from './app/screens/MypageScreen';
 import theme from './app/config/theme';
@@ -16,30 +17,32 @@ type tabBarIcon = keyof typeof tabBarIconDomain;
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            return <AntDesignIcon
-              name={tabBarIconDomain[route.name as tabBarIcon]}
-              size={size}
-              color={color}
-            />;
-          },
-          tabBarActiveTintColor: theme.colors.black,
-          tabBarInactiveTintColor: theme.colors.gray[400],
-        })}
-      >
-        <Tab.Screen
-          name="홈"
-          component={HomeScreen}
-        />
-        <Tab.Screen
-          name="내 정보"
-          component={MypageScreen}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              return <AntDesignIcon
+                name={tabBarIconDomain[route.name as tabBarIcon]}
+                size={size}
+                color={color}
+              />;
+            },
+            tabBarActiveTintColor: theme.colors.gray[200],
+            tabBarInactiveTintColor: theme.colors.gray[400],
+          })}
+        >
+          <Tab.Screen
+            name="홈"
+            component={HomeScreen}
+          />
+          <Tab.Screen
+            name="내 정보"
+            component={MypageScreen}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
---
+++ app/config/theme.ts
@@ -0,0 +1,31 @@
+import { DefaultTheme } from 'styled-components/native';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#54E58E",
+    secondary: "#8B74FF",
+    red: "#EB5757",
+    blue: '#2F80ED',
+    black: "#000",
+    white: "#FFF",
+    gray: {
+      100: "#333333",
+      200: "#4F4F4F",
+      300: "#828282",
+      400: "#BDBDBD",
+      500: "#E0E0E0",
+      600: "#F2F2F2",
+      700: "#F8F8F8",
+    }
+  },
+  fontSize: {
+    xxl: "24px",
+    xl: "20px",
+    lg: "18px",
+    md: "16px",
+    sm: "14px",
+    xs: "12px",
+  },
+}
+
+export default theme;
