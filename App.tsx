098f229e2862fb0bc29441072e136ce72432b631
Jiyoung Lim <swimjiy@gmail.com>
2021-10-13 13:17:13 +0900

    refactor: simplify directory and add nested navigator (#5)
    * refactor: simplify directory structure
    
    * feat: add nested navigator
    
    Co-authored-by: jiyoung.lim <swimjiy@krafton.com>

--- App.tsx
+++ App.tsx
@@ -1,48 +1,12 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 import { ThemeProvider } from 'styled-components/native';
-import HomeScreen from './app/screens/HomeScreen';
-import MypageScreen from './app/screens/MypageScreen';
+import AppNavigator from './app/navigators/AppNavigator';
 import theme from './app/config/theme';
-
-const Tab = createBottomTabNavigator();
-
-const tabBarIconDomain = {
-  '홈': 'home',
-  '내 정보': 'user',
-} as const;
-type tabBarIcon = keyof typeof tabBarIconDomain;
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Home"
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              return <AntDesignIcon
-                name={tabBarIconDomain[route.name as tabBarIcon]}
-                size={size}
-                color={color}
-              />;
-            },
-            tabBarActiveTintColor: theme.colors.gray[200],
-            tabBarInactiveTintColor: theme.colors.gray[400],
-          })}
-        >
-          <Tab.Screen
-            name="홈"
-            component={HomeScreen}
-          />
-          <Tab.Screen
-            name="내 정보"
-            component={MypageScreen}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <AppNavigator />
     </ThemeProvider>
   );
 }
---
+++ app/navigators/MainBottomParams.ts
@@ -0,0 +1,4 @@
+export type MainBottomParams = {
+  Home: undefined;
+  Mypage: undefined;
+}
