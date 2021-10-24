af9a2b1f38b26c2da594fb6f17b197310e757d81
jiyoung.lim <swimjiy@gmail.com>
2021-10-24 14:17:35 +0900

    feat: remove styled-components

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components/native';
 import { NativeBaseProvider } from 'native-base';
 import AppNavigator from './app/navigators/AppNavigator';
 import theme from './app/config/theme';
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <NativeBaseProvider>
-        <AppNavigator />
-      </NativeBaseProvider>
-    </ThemeProvider>
+    <NativeBaseProvider theme={theme}>
+      <AppNavigator />
+    </NativeBaseProvider>
   );
 }
