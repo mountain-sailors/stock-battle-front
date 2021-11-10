5fab78493b2410c2ee942d65fd1210824561d049
jiyoung.lim <swimjiy@gmail.com>
2021-11-11 03:27:13 +0900

    feat: add api functions

--- app/config/api.ts
+++ app/config/api.ts
@@ -1,1 +1,24 @@
-export const API_ENDPOINT = 'http://3.35.248.3:3000/';
+import useSWR from 'swr';
+import * as SecureStore from 'expo-secure-store';
+
+export const API_ENDPOINT = 'http://3.38.84.94:3000/api';
+
+type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+export async function callAPI(url: string, method: Method, body: any) {
+  const token = await SecureStore.getItemAsync('token');
+  const res = await fetch(`${API_ENDPOINT}${url}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}` ?? '',
+    },
+    body: JSON.stringify(body),
+  });
+  return res;
+}
+
+export function useGetRequest(url: string) {
+  return useSWR(url, (u: string) =>
+    callAPI(u, 'GET', undefined).then((res) => res.json()),
+  );
+}
