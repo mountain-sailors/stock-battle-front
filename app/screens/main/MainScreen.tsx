f88ffc4a7c789080518958be706ee65a707067e2
Chaewon Kang <saturday.the17th@gmail.com>
2021-11-21 00:12:52 +0900

    fix/minor issues

--- app/screens/main/MainScreen.tsx
+++ app/screens/main/MainScreen.tsx
@@ -22,12 +22,81 @@ const MainScreen: React.FC<MainSceenProp> = ({ navigation }) => {
   const userInfo = useGetRequest('/me').data;
   if (!roomList || !userInfo) return null;
 
-  const rankList = roomList
-    .filter((room: any) => room.gameStatus !== 0)
-    .map((room: any) => room.rank);
-  const average =
-    rankList.reduce((acc: number, cur: number) => acc + cur, 0) /
-    rankList.length;
+  if (roomList.length > 0) {
+    const rankList = roomList
+      .filter((room: any) => room.gameStatus !== 0)
+      .map((room: any) => room.rank);
+
+    const average =
+      rankList.length === 0
+        ? 0
+        : rankList.reduce((acc: number, cur: number) => acc + cur, 0) /
+          rankList.length;
+
+    return (
+      <Layout color="gray.100">
+        <VStack mb="19px">
+          <Box mx={-6} mt={-6} px={6} pb={6} bgColor="white">
+            <Flex direction="column" justify="space-between" mt={10} mb={6}>
+              <Flex direction="row">
+                <Text fontSize="3xl" fontWeight="bold">
+                  {`${userInfo.username}님의\n배틀 현황입니다`}
+                </Text>
+                <Image
+                  mt="7"
+                  ml="2"
+                  size="40px"
+                  source={require('./images/horse.png')}
+                  alt="icon"
+                />
+              </Flex>
+              <Text mt={2} fontSize="xl">
+                평균 등수
+                <Text fontSize="xl" fontWeight="bold" color="secondary.400">
+                  &nbsp; {average}위
+                </Text>
+              </Text>
+            </Flex>
+          </Box>
+        </VStack>
+        <Spacer />
+        <Box pb="20px">
+          <VStack space="3">
+            {roomList.map((room: any, index: number) => {
+              return {
+                // 다른 페이지로 넘어갈 때 roomId 같이 넘겨줘야할듯?
+                // 방이 취소 됐을 때 (CANCELLED) 처리 필요
+                NOT_STARTED: (
+                  <TouchableHighlight
+                    key={index}
+                    onPress={() => navigation.navigate('WaitingRoom')}
+                  >
+                    <WaitingRoomInfo key={index} room={room} />
+                  </TouchableHighlight>
+                ),
+                IN_PROGRESS: (
+                  <TouchableHighlight
+                    key={index}
+                    onPress={() => navigation.navigate('RunningRoom')}
+                  >
+                    <RunningRoomInfo key={index} room={room} />
+                  </TouchableHighlight>
+                ),
+                COMPLETED: (
+                  <TouchableHighlight
+                    key={index}
+                    onPress={() => navigation.navigate('ResultRoom')}
+                  >
+                    <ResultRoomInfo key={index} room={room} />
+                  </TouchableHighlight>
+                ),
+              }[gameStatusType[room.gameStatus]];
+            })}
+          </VStack>
+        </Box>
+      </Layout>
+    );
+  }
 
   return (
     <Layout color="gray.100">
@@ -47,49 +116,14 @@ const MainScreen: React.FC<MainSceenProp> = ({ navigation }) => {
               />
             </Flex>
             <Text mt={2} fontSize="xl">
-              평균 등수
               <Text fontSize="xl" fontWeight="bold" color="secondary.400">
-                &nbsp; {average}위
+                첫번째 게임을 시작해 보세요!
               </Text>
             </Text>
           </Flex>
         </Box>
       </VStack>
       <Spacer />
-      <Box pb="20px">
-        <VStack space="3">
-          {roomList.map((room: any, index: number) => {
-            return {
-              // 다른 페이지로 넘어갈 때 roomId 같이 넘겨줘야할듯?
-              // 방이 취소 됐을 때 (CANCELLED) 처리 필요
-              NOT_STARTED: (
-                <TouchableHighlight
-                  key={index}
-                  onPress={() => navigation.navigate('WaitingRoom')}
-                >
-                  <WaitingRoomInfo key={index} room={room} />
-                </TouchableHighlight>
-              ),
-              IN_PROGRESS: (
-                <TouchableHighlight
-                  key={index}
-                  onPress={() => navigation.navigate('RunningRoom')}
-                >
-                  <RunningRoomInfo key={index} room={room} />
-                </TouchableHighlight>
-              ),
-              COMPLETED: (
-                <TouchableHighlight
-                  key={index}
-                  onPress={() => navigation.navigate('ResultRoom')}
-                >
-                  <ResultRoomInfo key={index} room={room} />
-                </TouchableHighlight>
-              ),
-            }[gameStatusType[room.gameStatus]];
-          })}
-        </VStack>
-      </Box>
     </Layout>
   );
 };
