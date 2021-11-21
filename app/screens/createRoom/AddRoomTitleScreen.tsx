e415c1adb7085cd7b14d005da1e38c5714ed8dfe
Chaewon Kang <saturday.the17th@gmail.com>
2021-11-21 16:58:00 +0900

    fix/add room utils (copy clipboard, error toast)

--- app/screens/createRoom/AddRoomTitleScreen.tsx
+++ app/screens/createRoom/AddRoomTitleScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Box, Button, Input, Spacer, Text } from 'native-base';
+import { Box, Button, Input, Spacer, Text, useToast } from 'native-base';
 import { Layout } from '../../components';
 import { RootStackParams } from '../../navigators/RootStackParams';
 
@@ -8,6 +8,7 @@ type AddRoomTitleScreenProp = StackScreenProps<RootStackParams>;
 const AddRoomTitleScreen: React.FC<AddRoomTitleScreenProp> = ({
   navigation,
 }) => {
+  const toast = useToast();
   const [value, setValue] = React.useState('');
   const handleChange = (text: any) => setValue(text);
   return (
@@ -30,7 +31,17 @@ const AddRoomTitleScreen: React.FC<AddRoomTitleScreenProp> = ({
       <Spacer />
       <Button
         variant="solid"
-        onPress={() => navigation.navigate('AddRoomInfo', { roomName: value })}
+        onPress={() => {
+          if (value === '') {
+            toast.show({
+              status: 'error',
+              title: '방 제목',
+              description: '방 제목을 입력해주세요.',
+            });
+          } else {
+            navigation.navigate('AddRoomInfo', { roomName: value });
+          }
+        }}
       >
         다음
       </Button>
--- app/screens/createRoom/CompleteRoomScreen.tsx
+++ app/screens/createRoom/CompleteRoomScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
+import { Clipboard } from 'react-native';
 import { Button, Center, Flex, Text, VStack, Image, Spacer } from 'native-base';
 import { Layout } from '../../components';
 import { RootStackParams } from '../../navigators/RootStackParams';
@@ -10,6 +11,16 @@ const CompleteRoomScreen: React.FC<CompleteRoomScreenProp> = ({
   route,
 }) => {
   const { roomCode } = route.params;
+  const [copyText, setCopyText] = React.useState('초대코드 복사');
+
+  const copyToClipBoard = async (code: string) => {
+    Clipboard.setString(code);
+    try {
+      await Clipboard.getString();
+      setCopyText('초대코드가 복사되었습니다.');
+    } catch (err) {}
+  };
+
   return (
     <Layout>
       <Flex align="center" mt="20">
@@ -22,15 +33,15 @@ const CompleteRoomScreen: React.FC<CompleteRoomScreenProp> = ({
           {`방 생성이 완료되었습니다!\n초대 코드를 공유해 친구들과 시작해보세요.`}
         </Text>
         <Center w="full" mt={4} py="3" bgColor="gray.100" borderRadius="lg">
-          <Text fontSize="md" color="black">
+          <Text fontSize="md" color="black" px={3}>
             {roomCode}
           </Text>
         </Center>
       </Flex>
       <Spacer />
       <VStack space="2">
-        <Button variant="outline" onPress={() => navigation.navigate('Main')}>
-          초대코드 복사
+        <Button variant="outline" onPress={() => copyToClipBoard(roomCode)}>
+          {copyText}
         </Button>
         <Button variant="solid" onPress={() => navigation.navigate('Main')}>
           메인으로 이동
