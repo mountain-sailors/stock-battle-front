5f9f94e8c5dc473810c99a78a349c7a9a31e4f7e
Chaewon Kang <ilpermet@gmail.com>
2021-11-23 16:32:27 +0900

    fix: add room utils (#38)
    * fix/add room utils (copy clipboard, error toast)
    
    * bug/add toast when copy success or fail
    
    Co-authored-by: Chaewon Kang <saturday.the17th@gmail.com>

--- app/screens/createRoom/CompleteRoomScreen.tsx
+++ app/screens/createRoom/CompleteRoomScreen.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Button, Center, Flex, Text, VStack, Image, Spacer } from 'native-base';
+import { Clipboard } from 'react-native';
+import {
+  Button,
+  Center,
+  Flex,
+  Text,
+  VStack,
+  Image,
+  Spacer,
+  useToast,
+} from 'native-base';
 import { Layout } from '../../components';
 import { RootStackParams } from '../../navigators/RootStackParams';
 
@@ -10,6 +20,26 @@ const CompleteRoomScreen: React.FC<CompleteRoomScreenProp> = ({
   route,
 }) => {
   const { roomCode } = route.params;
+  const toast = useToast();
+
+  const copyToClipBoard = async (code: string) => {
+    Clipboard.setString(code);
+    try {
+      await Clipboard.getString();
+      toast.show({
+        status: 'success',
+        title: '복사 성공',
+        description: '초대코드가 복사되었습니다.',
+      });
+    } catch {
+      toast.show({
+        status: 'error',
+        title: '에러',
+        description: '초대코드 복사 중 문제가 발생했습니다.',
+      });
+    }
+  };
+
   return (
     <Layout>
       <Flex align="center" mt="20">
@@ -22,14 +52,14 @@ const CompleteRoomScreen: React.FC<CompleteRoomScreenProp> = ({
           {`방 생성이 완료되었습니다!\n초대 코드를 공유해 친구들과 시작해보세요.`}
         </Text>
         <Center w="full" mt={4} py="3" bgColor="gray.100" borderRadius="lg">
-          <Text fontSize="md" color="black">
+          <Text fontSize="md" color="black" px={3}>
             {roomCode}
           </Text>
         </Center>
       </Flex>
       <Spacer />
       <VStack space="2">
-        <Button variant="outline" onPress={() => navigation.navigate('Main')}>
+        <Button variant="outline" onPress={() => copyToClipBoard(roomCode)}>
           초대코드 복사
         </Button>
         <Button
