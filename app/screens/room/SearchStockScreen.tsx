4f5f8f2fe653dfb706df680f519adaa8158cc128
jiyoung.lim <swimjiy@gmail.com>
2021-11-24 00:01:17 +0900

    feat: add search stock logic

--- app/screens/room/SearchStockScreen.tsx
+++ app/screens/room/SearchStockScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Box, Heading, HStack, Pressable, VStack } from 'native-base';
+import { Heading, HStack, Pressable, VStack, Text, Spacer } from 'native-base';
 
 import { Layout, SearchInput } from '../../components';
 import { RootStackParams } from '../../navigators/RootStackParams';
@@ -19,13 +19,9 @@ const SearchStockScreen: React.FC<SearchStockScreenProp> = ({
 }) => {
   const { roomId } = route.params;
   const [searchKeyword, setSearchKeyword] = React.useState('');
-  const handleChange = (event: any) => setSearchKeyword(event.nativeEvent.text);
-  // const getColorBySign = (value: number) => {
-  //   const sign = Math.sign(value);
-  //   if (sign === 1) return 'red.400';
-  //   if (sign === -1) return 'blue.400';
-  //   return 'black';
-  // };
+  const handleChange = (event: any) => {
+    setSearchKeyword(event.nativeEvent.text);
+  };
   const stockList = useGetRequest('/stock').data;
   if (!stockList) return null;
 
@@ -38,29 +34,35 @@ const SearchStockScreen: React.FC<SearchStockScreenProp> = ({
         handleChange={handleChange}
       />
       <VStack mt={4} space={4}>
-        {stockList.map((v: Stock) => (
-          <Pressable
-            key={v.ticker}
-            onPress={() =>
-              // TODO: 주식 등록에 필요한 값 확인 후 object 형태로 params로 전달하기
-              navigation.navigate('RegisterStock', {
-                stockName: v.ticker,
-                roomId,
-              })
-            }
-          >
-            <HStack space={2} alignItems="center">
-              <Heading flex={1} fontSize="md" color="black">
-                {v.ticker}
-              </Heading>
-              <Box>
-                <Heading flex={1} fontSize="md" textAlign="right" color="black">
-                  {`$${v.price}`}
+        {stockList
+          .filter((v: { ticker: string }) =>
+            v.ticker.toLowerCase().includes(searchKeyword.toLowerCase()),
+          )
+          .map((v: Stock) => (
+            <Pressable
+              key={v.ticker}
+              onPress={() =>
+                // TODO: 주식 등록에 필요한 값 확인 후 object 형태로 params로 전달하기
+                navigation.navigate('RegisterStock', {
+                  stockName: v.ticker,
+                  roomId,
+                })
+              }
+              mx={-4}
+              px={4}
+              _pressed={{ bgColor: 'gray.100' }}
+            >
+              <HStack space={2} alignItems="center" py={2}>
+                <Heading fontSize="md" color="black">
+                  {v.ticker}
                 </Heading>
-              </Box>
-            </HStack>
-          </Pressable>
-        ))}
+                <Spacer />
+                <Text fontSize="md" textAlign="right" color="gray.400">
+                  {`$${v.price}`}
+                </Text>
+              </HStack>
+            </Pressable>
+          ))}
       </VStack>
     </Layout>
   );
