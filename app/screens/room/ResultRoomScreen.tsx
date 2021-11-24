7f5bc4e637f0097eb181517d14116c4932caef4c
Chaewon Kang <ilpermet@gmail.com>
2021-11-24 13:58:40 +0900

    feat: result room API sync (#44)
    * feat/add dummy result room page to presentation
    
    * feat: result and player info API sync
    
    Co-authored-by: Chaewon Kang <saturday.the17th@gmail.com>

--- app/screens/room/ResultRoomScreen.tsx
+++ app/screens/room/ResultRoomScreen.tsx
@@ -16,42 +16,73 @@ import { RootStackParams } from '../../navigators/RootStackParams';
 import { callAPI } from '../../config/api';
 import { IMAGE_URL } from '../../config/consts';
 
-const DUMMY_DATA = [
-  {
-    userName: 'admin',
-    ticker: 'APPL',
-    value: 12,
-    avatar: IMAGE_URL[0],
-  },
-  {
-    userName: '김주주',
-    ticker: 'BYND',
-    value: 5.5,
-    avatar: IMAGE_URL[1],
-  },
-  {
-    userName: '정감자',
-    ticker: 'TSLA',
-    value: -1.3,
-    avatar: IMAGE_URL[2],
-  },
-];
+type Result = {
+  userId: number;
+  isWin: number;
+  rank: number;
+  profit: number;
+  ticker: string;
+  amount: number;
+};
 
-type ResultRoomScreenProp = StackScreenProps<RootStackParams, 'CompleteRoom'>;
+type Player = {
+  id: number;
+  username: string;
+  avatar: string;
+  color: string;
+};
+
+type ResultRoomScreenProp = StackScreenProps<RootStackParams, 'ResultRoom'>;
 const ResultRoomScreen: React.FC<ResultRoomScreenProp> = ({ route }) => {
   const { roomId } = route.params;
+  const [resultData, setResultData] = React.useState<Result[]>([]);
+  const [playerData, setPlayerData] = React.useState<Player[]>([]);
+  const [myData, setMyData] = React.useState({
+    userId: 0,
+    userName: '',
+    userEmail: '',
+    point: 0,
+    avatar: '',
+  });
 
   React.useEffect(() => {
-    callAPI(`/game-history/room/${roomId}`, 'GET', undefined)
+    callAPI(`/me`, 'GET', undefined)
       .then((res) => res.json())
       .then((res) => {
         // WHAT TO DO with res
-        console.log(res);
+        setMyData(res);
+        console.log(`MyData:: ${myData}`);
+        console.log('================================');
       })
       .catch((err) => {
         console.error('Error: ', err);
       });
-  });
+
+    callAPI(`/game-history/room/${roomId}`, 'GET', undefined)
+      .then((res) => res.json())
+      .then((res) => {
+        // WHAT TO DO with res
+        setResultData(res);
+        console.log(`resultData:: ${resultData}`);
+        console.log(typeof resultData);
+        console.log('================================');
+      })
+      .catch((err) => {
+        console.error('Error: ', err);
+      });
+
+    callAPI(`/player/${roomId}`, 'GET', undefined)
+      .then((res) => res.json())
+      .then((res) => {
+        // WHAT TO DO with res
+        setPlayerData(res);
+        console.log(`playerData:: ${playerData}`);
+        console.log('================================');
+      })
+      .catch((err) => {
+        console.error('Error: ', err);
+      });
+  }, []);
 
   return (
     <Layout>
@@ -68,7 +99,11 @@ const ResultRoomScreen: React.FC<ResultRoomScreenProp> = ({ route }) => {
       <HStack mt={6} p={6} rounded="lg" bgColor="secondary.400">
         <Flex align="center">
           <Text fontSize="lg" fontWeight="bold" color="white">
-            1
+            {resultData
+              .filter((el) => {
+                if (el.userId === myData.userId) return el;
+              })
+              .map((item) => item.rank)}
           </Text>
           <Text fontSize="sm" color="gray.100">
             최종 순위
@@ -77,7 +112,12 @@ const ResultRoomScreen: React.FC<ResultRoomScreenProp> = ({ route }) => {
         <Spacer />
         <Flex align="center">
           <Text fontSize="lg" fontWeight="bold" color="white">
-            12%
+            {resultData
+              .filter((el) => {
+                if (el.userId === myData.userId) return el;
+              })
+              .map((item) => item.profit)}
+            %
           </Text>
           <Text fontSize="sm" color="gray.100">
             최종 수익률
@@ -86,7 +126,11 @@ const ResultRoomScreen: React.FC<ResultRoomScreenProp> = ({ route }) => {
         <Spacer />
         <Flex align="center">
           <Text fontSize="lg" fontWeight="bold" color="white">
-            APPL
+            {resultData
+              .filter((el) => {
+                if (el.userId === myData.userId) return el;
+              })
+              .map((item) => item.ticker)}
           </Text>
           <Text fontSize="sm" color="gray.100">
             종목
@@ -95,21 +139,30 @@ const ResultRoomScreen: React.FC<ResultRoomScreenProp> = ({ route }) => {
       </HStack>
       <FlatList
         mt={6}
-        data={DUMMY_DATA}
+        data={resultData}
         renderItem={({
           item,
           index,
         }: {
-          item: typeof DUMMY_DATA[number];
+          item: typeof resultData[number];
           index: number;
         }) => (
           <HStack alignItems="center" space={4} py={2}>
             <Box _text={{ fontSize: 'md', fontWeight: 'bold' }}>
               {index + 1}
             </Box>
-            <Avatar bg="gray.100" rounded="lg" source={item.avatar} p={2} />
+            <Avatar
+              bg="gray.100"
+              rounded="lg"
+              source={IMAGE_URL[index]}
+              p={2}
+            />
             <Box flex={1} _text={{ fontSize: 'md', fontWeight: 'bold' }}>
-              {item.userName}
+              {playerData
+                .filter((pl) => {
+                  if (pl.id === item.userId) return pl;
+                })
+                .map((pl) => pl.username)}
             </Box>
             <Box flex={1} _text={{ textAlign: 'right', fontSize: 'md' }}>
               {item.ticker}
@@ -120,10 +173,10 @@ const ResultRoomScreen: React.FC<ResultRoomScreenProp> = ({ route }) => {
                 textAlign: 'right',
                 fontSize: 'md',
                 fontWeight: 'bold',
-                color: Math.sign(item.value) === 1 ? 'red.400' : 'blue.400',
+                color: Math.sign(item.profit) === 1 ? 'red.400' : 'blue.400',
               }}
             >
-              {`${item.value}%`}
+              {`${item.profit}%`}
             </Box>
           </HStack>
         )}
