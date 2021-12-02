ace47acb4d342ffe302dd1654de012aefe07dce1
Chaewon Kang <saturday.the17th@gmail.com>
2021-12-02 23:37:34 +0900

    feat: record screen api sync

--- app/screens/record/RecordScreen.tsx
+++ app/screens/record/RecordScreen.tsx
@@ -15,11 +15,41 @@ import { Layout } from '../../components';
 import { RootStackParams } from '../../navigators/RootStackParams';
 import { useGetRequest } from '../../config/api';
 import { IMAGE_URL } from '../../config/consts';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 type RecordScreenProp = StackScreenProps<RootStackParams, 'Record'>;
 const RecordScreen: React.FC<RecordScreenProp> = () => {
   const meData = useGetRequest(`/me`).data;
   if (meData === undefined) return null;
+
+  const gameHistory = useGetRequest(
+    `/game-history/${meData.userId.toString()}`,
+  ).data;
+  console.log(gameHistory);
+
+  const rankList = gameHistory?.map((game: any) => game.rank) ?? [];
+  const average =
+    rankList.length === 0
+      ? 0
+      : Math.round(
+          rankList.reduce((acc: number, cur: number) => acc + cur, 0) /
+            rankList.length,
+        );
+
+  const winCount =
+    rankList.length === 0
+      ? 0
+      : rankList.filter((item: any) => {
+          if (item === 1) return item;
+        });
+
+  const winCountLen = winCount === 0 ? 0 : winCount.length;
+
+  let winRate = 0;
+
+  if (rankList.length === 0 || winCountLen === 0) winRate = 0;
+  else winRate = Math.round((winCountLen / rankList.length) * 100);
+
   return (
     <Layout>
       <VStack m={0} mt={5}>
@@ -49,41 +79,15 @@ const RecordScreen: React.FC<RecordScreenProp> = () => {
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        transform: [{ rotate: '180deg' }],
+                        transform: [{ rotate: '270deg' }],
                       }}
                     >
-                      <View
-                        style={{
-                          width: 75,
-                          height: 75,
-                          borderRadius: 37.5,
-                          backgroundColor: '#1A1B22',
-                          zIndex: 0,
-                        }}
-                      />
-                      <View
-                        style={{
-                          width: 37.5,
-                          height: 75,
-                          backgroundColor: '#54E68E',
-                          borderRadius: 37.5,
-                          borderTopRightRadius: 0,
-                          borderBottomRightRadius: 0,
-                          transform: [{ rotate: '0deg' }],
-                          position: 'absolute',
-                          zIndex: 1,
-                          alignSelf: 'flex-start',
-                        }}
-                      />
-                      <View
-                        style={{
-                          width: 65,
-                          height: 65,
-                          backgroundColor: '#1A1B22',
-                          borderRadius: 32.5,
-                          position: 'absolute',
-                          zIndex: 2,
-                        }}
+                      <AnimatedCircularProgress
+                        size={75}
+                        width={5}
+                        fill={Math.ceil((winRate / 10) * 10)}
+                        tintColor="#54E68E"
+                        backgroundColor="#3d5875"
                       />
                     </View>
                     <Circle size="60px" bg="#1A1B22"></Circle>
@@ -99,8 +103,8 @@ const RecordScreen: React.FC<RecordScreenProp> = () => {
                 </Box>
                 <Flex direction="column" w="70%">
                   <Flex direction="row">
-                    <Text color="white">승률</Text>{' '}
-                    <Text color="#54E68E">50%</Text>
+                    <Text color="white">승률</Text>
+                    <Text color="#54E68E">&nbsp;{winRate && winRate}%</Text>
                   </Flex>
                   <Text fontSize="2xl" color="white" fontWeight="bold">
                     {meData.username}
@@ -112,7 +116,7 @@ const RecordScreen: React.FC<RecordScreenProp> = () => {
                 <Box w="33.3%">
                   <Flex direction="column" justify="center" align="center">
                     <Text fontSize="2xl" color="white" fontWeight="bold">
-                      3
+                      {gameHistory && gameHistory.length}
                     </Text>
                     <Text fontSize="sm" color="white">
                       참여 게임수
@@ -122,7 +126,7 @@ const RecordScreen: React.FC<RecordScreenProp> = () => {
                 <Box w="33.3%">
                   <Flex direction="column" justify="center" align="center">
                     <Text fontSize="2xl" color="white" fontWeight="bold">
-                      2
+                      {average && average}
                     </Text>
                     <Text fontSize="sm" color="white">
                       평균 등수
@@ -132,7 +136,7 @@ const RecordScreen: React.FC<RecordScreenProp> = () => {
                 <Box w="33.3%">
                   <Flex direction="column" justify="center" align="center">
                     <Text fontSize="2xl" color="white" fontWeight="bold">
-                      1
+                      {winCountLen && winCountLen}
                     </Text>
                     <Text fontSize="sm" color="white">
                       1등 횟수
@@ -145,191 +149,91 @@ const RecordScreen: React.FC<RecordScreenProp> = () => {
           </Box>
         </Box>
       </VStack>
-      <Spacer />
 
       <Box pb={3} pt={3}>
         <VStack space="3">
           <Text fontSize="xl" fontWeight="bold">
             히스토리
           </Text>
-          <Box
-            w="100%"
-            bg="#fff"
-            pt="4"
-            pb="1"
-            borderTopWidth={1}
-            borderTopColor="#E0E0E0"
-          >
-            <Flex direction="row" w="100%" h="24px" align="flex-end">
-              <Box mr={2} w="40px" bg="#7B61FF" rounded="3">
-                <Flex
-                  h="100%"
-                  direction="column"
-                  align="center"
-                  justify="center"
+          {gameHistory &&
+            gameHistory.map((game: any) => {
+              return (
+                <Box
+                  key={game.title}
+                  w="100%"
+                  bg="#fff"
+                  pt="4"
+                  pb="1"
+                  borderTopWidth={1}
+                  borderTopColor="#E0E0E0"
                 >
-                  <Text fontSize="xs" fontWeight="bold" color="#fff">
-                    1위
-                  </Text>
-                </Flex>
-              </Box>
-              <Text
-                mr={2}
-                fontSize="xl"
-                fontWeight="bold"
-                color="black"
-                alignSelf="flex-start"
-                mt="-2px"
-              >
-                1위 치킨빵🍗 
-              </Text>
-              <Text fontSize="xs" color="#828282">
-                2021.10.03 ~ 2021.10.10
-              </Text>
-            </Flex>
-            <Spacer />
-            <Flex direction="row" justify="space-between" mt={5}>
-              <Box w="50%">
-                <Text fontSize="md" color="#828282">
-                  수익률
-                </Text>
-                <Text fontSize="md" color="#828282">
-                  참여종목
-                </Text>
-                <Text fontSize="md" color="#828282">
-                  참여인원
-                </Text>
-              </Box>
-              <Spacer />
-              <Box w="50%">
-                <Flex direction="column" justify="flex-end" align="flex-end">
-                  <Text fontSize="md">6.61%</Text>
-                  <Text fontSize="md">APPL</Text>
-                  <Text fontSize="md">admin, 김주주, 정감자</Text>
-                </Flex>
-              </Box>
-            </Flex>
-            <Spacer />
-          </Box>
-          <Box
-            w="100%"
-            bg="#fff"
-            pt="4"
-            pb="1"
-            borderTopWidth={1}
-            borderTopColor="#E0E0E0"
-          >
-            <Flex direction="row" w="100%" h="24px" align="flex-end">
-              <Box mr={2} w="40px" bg="#7B61FF" rounded="3" opacity={0.6}>
-                <Flex
-                  h="100%"
-                  direction="column"
-                  align="center"
-                  justify="center"
-                >
-                  <Text fontSize="xs" fontWeight="bold" color="#fff">
-                    2위
-                  </Text>
-                </Flex>
-              </Box>
-              <Text
-                mr={2}
-                fontSize="xl"
-                fontWeight="bold"
-                color="black"
-                alignSelf="flex-start"
-                mt="-2px"
-              >
-                담주 회식내기
-              </Text>
-              <Text fontSize="xs" color="#828282">
-                2021.10.06 ~ 2021.10.13
-              </Text>
-            </Flex>
-            <Spacer />
-            <Flex direction="row" justify="space-between" mt={5}>
-              <Box w="50%">
-                <Text fontSize="md" color="#828282">
-                  수익
-                </Text>
-                <Text fontSize="md" color="#828282">
-                  참여종목
-                </Text>
-                <Text fontSize="md" color="#828282">
-                  참여인원
-                </Text>
-              </Box>
-              <Spacer />
-              <Box w="50%">
-                <Flex direction="column" justify="flex-end" align="flex-end">
-                  <Text fontSize="md">+$77.88</Text>
-                  <Text fontSize="md">META</Text>
-                  <Text fontSize="md">admin, 임코코, 정감자</Text>
-                </Flex>
-              </Box>
-            </Flex>
-            <Spacer />
-          </Box>
-
-          <Box
-            w="100%"
-            bg="#fff"
-            pt="4"
-            pb="1"
-            borderTopWidth={1}
-            borderTopColor="#E0E0E0"
-          >
-            <Flex direction="row" w="100%" h="24px" align="flex-end">
-              <Box mr={2} w="40px" bg="#7B61FF" rounded="3" opacity={0.3}>
-                <Flex
-                  h="100%"
-                  direction="column"
-                  align="center"
-                  justify="center"
-                >
-                  <Text fontSize="xs" fontWeight="bold" color="#fff">
-                    3위
-                  </Text>
-                </Flex>
-              </Box>
-              <Text
-                mr={2}
-                fontSize="xl"
-                fontWeight="bold"
-                color="black"
-                alignSelf="flex-start"
-                mt="-2px"
-              >
-                주식 스터디팟
-              </Text>
-              <Text fontSize="xs" color="#828282">
-                2021.10.10 ~ 2021.10.17
-              </Text>
-            </Flex>
-            <Spacer />
-            <Flex direction="row" justify="space-between" mt={5}>
-              <Box w="50%">
-                <Text fontSize="md" color="#828282">
-                  변동폭
-                </Text>
-                <Text fontSize="md" color="#828282">
-                  참여종목
-                </Text>
-                <Text fontSize="md" color="#828282">
-                  참여인원
-                </Text>
-              </Box>
-              <Spacer />
-              <Box w="50%">
-                <Flex direction="column" justify="flex-end" align="flex-end">
-                  <Text fontSize="md">$10.25</Text>
-                  <Text fontSize="md">BYND</Text>
-                  <Text fontSize="md">admin, 강치치</Text>
-                </Flex>
-              </Box>
-            </Flex>
-            <Spacer />
-          </Box>
+                  <Flex direction="row" w="100%" h="24px" align="flex-end">
+                    <Box mr={2} w="40px" bg="#7B61FF" rounded="3">
+                      <Flex
+                        h="100%"
+                        direction="column"
+                        align="center"
+                        justify="center"
+                      >
+                        <Text fontSize="xs" fontWeight="bold" color="#fff">
+                          {game.rank}위
+                        </Text>
+                      </Flex>
+                    </Box>
+                    <Text
+                      mr={2}
+                      fontSize="xl"
+                      fontWeight="bold"
+                      color="black"
+                      alignSelf="flex-start"
+                      mt="-2px"
+                    >
+                      {game.title}
+                    </Text>
+                    <Text fontSize="xs" color="#828282">
+                      {game.startDate.slice(0, 10)} ~{' '}
+                      {game.endDate.slice(0, 10)}
+                    </Text>
+                  </Flex>
+                  <Spacer />
+                  <Flex direction="row" justify="space-between" mt={5}>
+                    <Box w="30%">
+                      <Text fontSize="md" color="#828282">
+                        수익률
+                      </Text>
+                      <Text fontSize="md" color="#828282">
+                        참여종목
+                      </Text>
+                      <Text fontSize="md" color="#828282">
+                        참여인원
+                      </Text>
+                    </Box>
+                    <Spacer />
+                    <Box w="70%">
+                      <Flex
+                        direction="column"
+                        justify="flex-end"
+                        align="flex-end"
+                      >
+                        <Text fontSize="md">{game.profit}</Text>
+                        <Text fontSize="md">{game.ticker}</Text>
+                        <Text fontSize="md">
+                          {game.players.map((player: any) => {
+                            if (
+                              game.players.indexOf(player) ===
+                              game.players.length - 1
+                            )
+                              return `${player}`;
+                            return `${player}, `;
+                          })}
+                        </Text>
+                      </Flex>
+                    </Box>
+                  </Flex>
+                  <Spacer />
+                </Box>
+              );
+            })}
         </VStack>
       </Box>
     </Layout>
