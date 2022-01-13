feca1336e4877e36514f26e997fb76a78c515f70
jiyoung.lim <swimjiy@gmail.com>
2022-01-14 02:21:57 +0900

    fix: fix agreement screen error

--- app/screens/initial/AgreementScreen.tsx
+++ app/screens/initial/AgreementScreen.tsx
@@ -23,7 +23,7 @@ const AGREEMENT_CONTENTS: AgreementContent[] = [
   },
 ];
 
-type AgreementScreenProp = StackScreenProps<RootStackParams, 'Init'>;
+type AgreementScreenProp = StackScreenProps<RootStackParams, 'Agreement'>;
 const AgreementScreen: React.FC<AgreementScreenProp> = ({ navigation }) => {
   const [showModal, setShowModal] = React.useState(false);
   const [isChecked, setIsChecked] = React.useState({
@@ -40,7 +40,12 @@ const AgreementScreen: React.FC<AgreementScreenProp> = ({ navigation }) => {
     <Layout>
       <VStack space={4}>
         {AGREEMENT_CONTENTS.map((v) => (
-          <Flex direction="row" justify="space-between" alignItems="center">
+          <Flex
+            key={v.title}
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
             <Checkbox
               isChecked={isChecked[v.type]}
               onChange={(isSelected) =>
