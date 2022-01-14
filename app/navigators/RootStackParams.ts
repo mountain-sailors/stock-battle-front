4c1040c6fbb611952b2109014407c06cf02157ea
jiyoung.lim <swimjiy@gmail.com>
2022-01-15 00:20:34 +0900

    refactor: align params list

--- app/navigators/RootStackParams.ts
+++ app/navigators/RootStackParams.ts
@@ -1,23 +1,33 @@
 export type RootStackParams = {
   Main: undefined;
   Home: undefined;
+  Mypage: undefined;
+
+  // initial
   Init: undefined;
   Login: undefined;
   SignUp: undefined;
   Agreement: undefined;
+  FindPassword: undefined;
+  SendTempPwd: { email: string };
+
+  // record
   Record: undefined;
   SearchProfile: undefined;
-  AddRoomCode: undefined;
-  EnterRoom: undefined;
+
+  // createRoom
   AddRoomTitle: undefined;
   AddRoomInfo: { roomName: string };
   CompleteRoom: { roomCode: string };
+
+  // enterRoom
+  AddRoomCode: undefined;
+  EnterRoom: undefined;
+
+  // room
   WaitingRoom: { roomId: number; username: string };
   RunningRoom: { roomId: number };
   ResultRoom: { roomId: number };
   RegisterStock: { stockName: string; roomId: number };
   SearchStock: { roomId: number };
-  Mypage: undefined;
-  FindPassword: undefined;
-  SendTempPwd: { email: string };
 };
